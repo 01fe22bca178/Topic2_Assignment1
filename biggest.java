af0aea25c9fdb015d5e65c6107f25ee8187d1e0a
01fe22bca178 <01fe22bca178@kletech.ac.in>
2024-01-29 23:37:49 +0530

    code for finding biggest number among three

---
+++ biggest.java
@@ -0,0 +1,27 @@
+/**
+ * The "biggest" class takes three numbers as input and outputs the largest number among them.
+ */
+import java.util.Scanner;
+
+/**
+ * The "biggest" class takes three numbers as input and outputs the largest number among them.
+ */
+public class biggest
+{
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter  the three numbers:");
+
+        double num1 = scanner.nextDouble();
+        double num2 = scanner.nextDouble();
+        double num3 = scanner.nextDouble();
+
+        double largest = Math.max(Math.max(num1, num2), num3);
+
+        System.out.println("The largest number is: " + largest);
+
+        scanner.close();
+    }
+}
+
